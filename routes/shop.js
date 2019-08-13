a266ba8ac8db32561d91b665d2b869dbf080968d
Renzo <rcnavarrop@gmail.com>
2019-08-13 17:03:53 -0500

    end of pug condiguration

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const path = require('path');
 
-const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/admin/shop'
+  });
 });
 
 module.exports = router;
