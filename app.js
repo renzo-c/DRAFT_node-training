2361616ebd05007d3aa9c34ce7732b3cf049c04f
Renzo <rcnavarrop@gmail.com>
2019-08-17 21:53:14 -0500

    finishing with product controller

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controller/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,10 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .render('404', { pageTitle: 'Page Not Found', layout: 'main-layout' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
