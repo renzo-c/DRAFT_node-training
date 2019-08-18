16c5266cf312cb4f565538362cb1038c12b2bd9c
Renzo <rcnavarrop@gmail.com>
2019-08-18 11:33:19 -0500

    product model added

--- controller/error.js
+++ controller/error.js
@@ -1,5 +1,5 @@
 exports.get404 = (req, res, next) => {
   res
     .status(404)
-    .render('404', { pageTitle: 'Page Not Found', layout: 'main-layout' });
+    .render('404', { pageTitle: 'Page Not Found', path: '', layout: 'main-layout' });
 };
