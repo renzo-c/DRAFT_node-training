4d96e0c9a30572d3fa56ab7804e04d6e37f2c12e
Renzo Navarro <rcnavarrop@gmail.com>
2022-06-25 15:13:21 -0400

    Implements crud actions

--- todo-manager-api/controllers/tasks.js
+++ todo-manager-api/controllers/tasks.js
@@ -1,21 +1,64 @@
-const getAllTasks = (req, res) => {
-  res.send("all items from getAllTasks");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(201).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findById({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` });
+    }
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` });
+    }
+    res.json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 module.exports = {
--- todo-manager-api/db/connect.js
+++ todo-manager-api/db/connect.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-const connectionString = process.env.DB_URL;
+const connectDB = (url) => {
+  return mongoose
+    .connect(url)
+    .then(() => console.log("Connected to the db..."))
+    .catch((err) => console.log(err));
+};
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("Connected to the db..."))
-  .catch((err) => console.log(err));
+module.exports = connectDB;
